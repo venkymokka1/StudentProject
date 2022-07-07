97ad7832ecc8e6433a9ce75a294607124252a2df
mokka.venkateswarao <mokka.venkateswarao@aviacode.com>
2022-07-07 06:49:04 +0530

    Changed text in the console.

--- StudentProject/Program.cs
+++ StudentProject/Program.cs
@@ -6,7 +6,7 @@ namespace StudentProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Venkat Mokka!");
         }
     }
 }
