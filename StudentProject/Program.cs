21b29ce76eac3a0a43f3e7714d380cff938af98a
mokka.venkateswarao <mokka.venkateswarao@aviacode.com>
2022-07-15 05:26:11 +0530

    Added

--- StudentProject/Program.cs
+++ StudentProject/Program.cs
@@ -7,17 +7,17 @@ namespace StudentProject
     {
         static void Main()
         {
-
             Console.WriteLine("Hello Abid! in new branch");
-
             Console.WriteLine("Hello Venky & Abid! in new branch");
             Console.WriteLine("Hello Abid & Venky! in new branch");
-            string sourcePath = "D:\\Venkat\\Test";
+            string sourcePath = "D:\\Venkat\\Hvrad\\Premier\\From Premier To Gebbs";
+            string desPath = "D:\\Venkat\\Hvrad\\Premier\\POSITION-BY_Rpt\\FTP";
             string test = "Hello Venky & Abid! in new branch";
             // text write
-            File.WriteAllText(sourcePath + ".txt", test);
+            File.WriteAllText(sourcePath + "\\Test.txt", test);
             // text Read
-            string readText = File.ReadAllText(sourcePath + ".txt");
+            string readText = File.ReadAllText(sourcePath + "\\Test.txt");
+            Console.WriteLine("txt file Read & Write");
         }
     }
 }
