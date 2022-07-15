8fcf5bc232768ebdef48730bafe7e66f660bab32
Venkat Moka <venkim09@gmail.com>
2022-07-15 06:00:13 +0530

    New Project

--- StudentProject/Student.cs
+++ StudentProject/Student.cs
@@ -8,17 +8,7 @@ namespace StudentProject
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Hello Venkat Mokka");
-            Console.WriteLine("Hello Abid Basha");
-       
-            for (int i = 0; i < 100; i++)
-            {
-                Console.WriteLine("Latest commit");
-                Console.WriteLine("Changed this line");
-                Console.WriteLine(i + 1);
-                Console.WriteLine(i + 2);
-            }
+            Console.WriteLine("New project");
         }
     }
 }
