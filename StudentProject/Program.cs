400e710c00b2bb3be9a48d9619fc030c38ce0294
mokka.venkateswarao <mokka.venkateswarao@aviacode.com>
2022-07-21 07:15:14 +0530

    Added zip to unzip code

--- StudentProject/Program.cs
+++ StudentProject/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.IO.Compression;
+using System.Linq;
 
 namespace StudentProject
 {
@@ -7,8 +9,8 @@ namespace StudentProject
     {
         static void Main()
         {
-            string sourcePath = "C:\\Venkat\\sourcePath";
-            string desPath = "C:\\Venkat\\desPath";
+            string sourcePath = "D:\\Venkat123\\sourcePath";
+            string desPath = "D:\\Venkat123\\desPath";
             // Check the sourcePath exist
             if (!Directory.Exists(sourcePath))
                 Directory.CreateDirectory(sourcePath);
@@ -16,30 +18,43 @@ namespace StudentProject
             if (!Directory.Exists(desPath))
                 Directory.CreateDirectory(desPath);
 
-            string txt = "Hello Venky & Abid! in new branch";
+
+
+            //string txt = "Hello Venky & Abid! in new branch";
             string fileName = "Test.txt";
-            // File Write
-            if (!File.Exists(sourcePath + "\\" + fileName))
-                File.WriteAllText(sourcePath + "\\" + fileName, txt);
+            //// File Write
+            //if (!File.Exists(sourcePath + "\\" + fileName))
+            //    File.WriteAllText(sourcePath + "\\" + fileName, txt);
             // Getting files from sourcePath
             DirectoryInfo objDirInfo = new DirectoryInfo(sourcePath);
             FileInfo[] objInputFiles = objDirInfo.GetFiles();
             foreach (var item in objInputFiles)
             {
                 fileName = item.Name;
+                if (fileName.Contains(".zip"))
+                {
+                    //// File Copy
+                    //if (!File.Exists(desPath + "\\" + fileName))
+                    //    File.Copy(sourcePath + "\\" + fileName, desPath + "\\" + fileName);
 
-                // File Copy
-                if (!File.Exists(desPath + "\\" + fileName))
-                    File.Copy(sourcePath + "\\" + fileName, desPath + "\\" + fileName);
+                    // File Move
+                    if (!File.Exists(desPath + "\\" + fileName))
+                        File.Move(sourcePath + "\\" + fileName, desPath + "\\" + fileName);
+                    ZipFile.ExtractToDirectory(desPath + "\\" + fileName, desPath, true);
+                    DirectoryInfo oDirInfo = new DirectoryInfo(desPath);
+                    FileInfo[] oFCol = new FileInfo[0];
+                    oFCol = oDirInfo.GetFiles("*.pdf");
+                    int totalFiles = oFCol.Count();
+                    foreach (var item1 in oFCol)
+                    {
+                        File.WriteAllText(desPath + "\\" + item1.Name.Replace(".pdf", ".txt"), item1.Name);
+                    }
+                    //// File Read
+                    //string readText = File.ReadAllText(desPath + "\\" + fileName);
+                    //string[] readAllText = readText.Split(" "); // Thre is no comma in the above text, so we splited by single space only.
 
-                // File Move
-                if (!File.Exists(desPath + "\\" + fileName))
-                    File.Move(sourcePath + "\\" + fileName, desPath + "\\" + fileName);
-                // File Read
-                string readText = File.ReadAllText(desPath + "\\" + fileName);
-                string[] readAllText = readText.Split(" "); // Thre is no comma in the above text, so we splited by single space only.
-
-                Console.WriteLine(readText);
+                    //Console.WriteLine(readText);
+                }
             }
             Console.ReadLine();
         }
