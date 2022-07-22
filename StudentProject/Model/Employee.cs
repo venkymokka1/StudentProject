8ef25a5fb7d71664fc75051bf87b8650752a3bbb
mokka.venkateswarao <mokka.venkateswarao@aviacode.com>
2022-07-22 07:10:03 +0530

    Added logic for pdf, txt,doc,csv

---
+++ StudentProject/Model/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentProject.Model
+{
+    public class Employee
+    {
+        public int Code { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PolicyNo { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string ValidUp { get; set; }
+        public string Insurence { get; set; }
+    }
+}
--- StudentProject/Program.cs
+++ StudentProject/Program.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using StudentProject.Model;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using System.Text;
+using UglyToad.PdfPig;
 
 namespace StudentProject
 {
@@ -9,20 +13,22 @@ namespace StudentProject
     {
         static void Main()
         {
+            List<Employee> lst = new List<Employee>();
             string sourcePath = "D:\\Venkat123\\sourcePath";
             string desPath = "D:\\Venkat123\\desPath";
+            string archivePath = "D:\\Venkat123\\archivePath";
             // Check the sourcePath exist
             if (!Directory.Exists(sourcePath))
                 Directory.CreateDirectory(sourcePath);
             // Check the desPath exist
             if (!Directory.Exists(desPath))
                 Directory.CreateDirectory(desPath);
-
-
-
+            // Check the archivePath exist
+            if (!Directory.Exists(archivePath))
+                Directory.CreateDirectory(archivePath);
+            string fileName = null;
+            // Normal text to Write into txt file
             //string txt = "Hello Venky & Abid! in new branch";
-            string fileName = "Test.txt";
-            //// File Write
             //if (!File.Exists(sourcePath + "\\" + fileName))
             //    File.WriteAllText(sourcePath + "\\" + fileName, txt);
             // Getting files from sourcePath
@@ -31,32 +37,86 @@ namespace StudentProject
             foreach (var item in objInputFiles)
             {
                 fileName = item.Name;
+                //if (fileName.Contains(".zip"))
+                //{
+                //// File Copy
+                //if (!File.Exists(desPath + "\\" + fileName))
+                //    File.Copy(sourcePath + "\\" + fileName, desPath + "\\" + fileName);
+
+                // File Move
+                if (!File.Exists(desPath + "\\" + fileName))
+                    File.Move(sourcePath + "\\" + fileName, desPath + "\\" + fileName);
+                // Zip extract
                 if (fileName.Contains(".zip"))
-                {
-                    //// File Copy
-                    //if (!File.Exists(desPath + "\\" + fileName))
-                    //    File.Copy(sourcePath + "\\" + fileName, desPath + "\\" + fileName);
-
-                    // File Move
-                    if (!File.Exists(desPath + "\\" + fileName))
-                        File.Move(sourcePath + "\\" + fileName, desPath + "\\" + fileName);
                     ZipFile.ExtractToDirectory(desPath + "\\" + fileName, desPath, true);
-                    DirectoryInfo oDirInfo = new DirectoryInfo(desPath);
-                    FileInfo[] oFCol = new FileInfo[0];
+                // Get Files from path
+                DirectoryInfo oDirInfo = new DirectoryInfo(desPath);
+                FileInfo[] oFCol = new FileInfo[0];
+                if (fileName.Contains(".zip"))
                     oFCol = oDirInfo.GetFiles("*.pdf");
-                    int totalFiles = oFCol.Count();
-                    foreach (var item1 in oFCol)
+                else
+                    oFCol = oDirInfo.GetFiles();
+                int totalFiles = oFCol.Count();
+                string txt = null;
+                foreach (var item1 in oFCol)
+                {
+                    Employee mdl = new Employee();
+                    if (item1.Name.Contains(".pdf"))
                     {
-                        File.WriteAllText(desPath + "\\" + item1.Name.Replace(".pdf", ".txt"), item1.Name);
+                        txt = ExtractTextFromPdf(item1.FullName);
+                        if (fileName.Contains(".zip"))
+                            File.WriteAllText(item1.FullName.Replace(".pdf", ".txt"), txt);
                     }
-                    //// File Read
-                    //string readText = File.ReadAllText(desPath + "\\" + fileName);
-                    //string[] readAllText = readText.Split(" "); // Thre is no comma in the above text, so we splited by single space only.
-
-                    //Console.WriteLine(readText);
+                    else if (item1.Name.Contains(".txt"))
+                    {
+                        txt = File.ReadAllText(desPath + "\\" + fileName);
+                    }
+                    else if (item1.Name.Contains(".doc"))
+                    {
+                        txt = File.ReadAllText(desPath + "\\" + fileName);
+                    }
+                    else if (item1.Name.Contains(".csv"))
+                    {
+                        txt = File.ReadAllText(desPath + "\\" + fileName);
+                    }
+                    string[] res = txt.Split(" ");
+                    mdl.FirstName = res[99];
+                    mdl.LastName = res[100];
+                    mdl.Code = Convert.ToInt32(res[182]);
+                    mdl.Gender = res[135];
+                    mdl.Age = Convert.ToInt32(res[136]);
+                    mdl.PolicyNo = res[49];
+                    mdl.ValidUp = res[204];
+                    mdl.Insurence = res[7] + res[8] + res[9];
+                    // File Move from dest path to archive Path
+                    if (!File.Exists(archivePath + "\\" + fileName))
+                        File.Move(desPath + "\\" + fileName, archivePath + "\\" + fileName);
+                    lst.Add(mdl);
                 }
+                //// File Read
+                //string readText = File.ReadAllText(desPath + "\\" + fileName);
+                //string[] readAllText = readText.Split(" "); 
+                // Thre is no comma in the above text, so we splited by single space only.
+
+                //Console.WriteLine(readText);
+                //}
             }
             Console.ReadLine();
+        }
+
+        public static string ExtractTextFromPdf(string path)
+        {
+            using (var stream = File.OpenRead(path))
+            using (PdfDocument document = PdfDocument.Open(stream))
+            {
+                StringBuilder text = new StringBuilder();
+                for (int i = 1; i <= document.NumberOfPages; i++)
+                {
+                    var page = document.GetPage(i);
+                    text.Append(string.Join(" ", page.GetWords()));
+                }
+                return text.ToString();
+            }
         }
     }
 }
