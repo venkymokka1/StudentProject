55b5527596a012484430fa77de59f3707e06b801
Shaik Abidbasha <abidbashashaik452@gmail.com>
2022-07-25 05:33:06 +0530

    Code changed

--- StudentProject/Student.cs
+++ StudentProject/Student.cs
@@ -1,24 +1,25 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-//namespace StudentProject
-//{
-//    public class Student
-//    {
-//        static void Main(string[] args)
-//        {
+namespace StudentProject
+{
+    public class Student
+    {
+        static void Main(string[] args)
+        {
 
-//            Console.WriteLine("Hello Venkat Mokka");
-//            Console.WriteLine("Hello Abid Basha");
-       
-//            for (int i = 0; i < 100; i++)
-//            {
-//                Console.WriteLine("Latest commit");
-//                Console.WriteLine("Changed this line");
-//                Console.WriteLine(i + 1);
-//                Console.WriteLine(i + 2);
-//            }
-//        }
-//    }
-//}
+            Console.WriteLine("Hello Venkat Mokka");
+            Console.WriteLine("Hello Abid Basha");
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Latest commit");
+                Console.WriteLine("Changed this line");
+                Console.WriteLine(i + 1);
+                Console.WriteLine(i + 2);
+                Console.WriteLine("Code Changed");
+            }
+        }
+    }
+}
